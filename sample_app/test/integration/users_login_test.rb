6b77c74d7da7011d8068d94ed0adf3f38895a135
BirjendraRawat <rbirjendra@yahoo.com>
2024-03-28 17:11:46 +0530

    advance login implementation

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -28,7 +28,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid email/password" do
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "@user.email", password: "invalid" } }
@@ -58,6 +58,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assign(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again to verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 
 end
