2829850dbe2852cd0b56554221d119eb3301aead
RVJ MAC MINI <rvjmacmini@RVJs-MacBook-Air.local>
2024-04-24 18:16:47 +0530

    admin functionality to delete users

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       # if user&.authenticate(params[:session][:password])
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      # reset_session
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       # log_in @user
       redirect_back_or user
       else
--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -9,12 +9,19 @@
 #   end
 
 # Create a main sample user.
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
+User.create!(name: "Example User",
+             email: "example@railstutorial.org",
+             password: "foobar",
+             password_confirmation: "foobar",
+             admin: true)
 
 # Generate a bunch of additional users.
-199.times do |n|
+99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
 end
